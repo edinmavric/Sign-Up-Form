065f8ca896dd9f2b0e462aa88d1a18e8351de4d1
edinmavric <edinmavric10@gmail.com>
2023-02-10 20:10:00 +0100

    added more function

--- assets/app.js
+++ assets/app.js
@@ -1,33 +1,77 @@
+let forma = document.getElementById('form1');
+let button = document.getElementById('btn');
+
 let password = document.getElementById('pname');
 let confirmPassword = document.getElementById('cpname');
 let email = document.getElementById('ename');
 let confirmEmail = document.getElementById('cename');
-let nameAndSurname = document.getElementById('nameSurName');
 
-function validateForm() {
+let emailError = document.getElementById('email-error');
+let confirmEmailError = document.getElementById('cemail-error');
+let passError = document.getElementById('pass-error');
+let confirmPassError = document.getElementById('cpass-error');
 
+
+const validateForm = () => {
+
+    greenValidateForm();
+    
     if (email.value.length < 9) {
-        alert('Email must be entered!');
+        emailError.innerHTML = 'Email is incorrect!'
+        email.style.border = '1px solid red'
         return false;
-    }
+    } 
+    
     if (email.value.length != confirmEmail.value.length) {
-        alert('Confirm same email or use another one!');
+        confirmEmailError.innerHTML = 'Use same Email info!'
+        confirmEmail.style.border = '1px solid red'
         return false;
-    }
+    } 
+    
     if (password.value.length < 8) {
-        alert('Password must be atleast 8 characters long!');
+        passError.innerHTML = 'Password must contain 8 characters!'
+        password.style.border = "1px solid red";
         return false;
-    }
+    } 
+    
     if (password.value.length == confirmPassword.value.length) {
         prompt("Succesfull Sing up!", "Enter your username");
         return true;
     }
+    
     if (password.value.length != confirmPassword.value.length) {
-        alert("Passwords don't match!");
+        confirmPassError.innerHTML = "Passwords doesn't match!";
+        confirmPassword.style.border = "1px solid red";
         return false;
+    }
+}
+
+const greenValidateForm = () => {
+
+    if (email.value.length >= 9) {
+        emailError.innerHTML = 'Email is correct!'
+        email.style.border = '1px solid green'
+        return true;
+    } 
+    
+    if (email.value.length == confirmEmail.value.length) {
+        confirmEmailError.innerHTML = 'Email info matches!'
+        confirmEmail.style.border = '1px solid green'
+        return true;
+    } 
+
+    if (password.value.length >= 8) {
+        passError.innerHTML = 'Password can be used!'
+        password.style.border = "1px solid green";
+        return true;
+    } 
+
+    if (password.value.length == confirmPassword.value.length) {
+        confirmPassError.innerHTML = "Passwords match!";
+        confirmPassword.style.border = "1px solid green";
+        return true;
     }
 
 }
 
-// kad je tacan input je zelen kad nije onda je crven
-// ako je netacan da pise ispod inputa umesto alerta
+// button.addEventListener('submit', validateForm);
